6b3e984fe0328aed56f59143086cedb739c9ff01
Mark Florence <mflo999@gmail.com>
2025-11-24 08:31:45 -0800

    Reasonable test for bus.go

--- src/emulator/core/bus_test.go
+++ src/emulator/core/bus_test.go
@@ -12,8 +12,46 @@ func TestNewBus(t *testing.T) {
 		assert.Fail(t, "SubInit() should not be called here")
 	})
 	expected := []byte{0x01, 0x02, 0x03}
-	bus.SubOutbound(func(actual []byte) {
-		assert.Equal(t, expected, actual, "SubOutbound() received correctly")
+	var actual []byte
+	bus.SubOutbound(func(chars []byte) {
+		actual = chars
 	})
 	bus.PubOutbound(expected)
+	assert.Equal(t, expected, actual, "smoke test passed")
 }
+
+func TestPubSubPanic(t *testing.T) {
+	bus := NewBus()
+	expected := "HELP!"
+	var actual string
+	bus.SubPanic(func(msg string) {
+		actual = msg
+	})
+	bus.PubPanic(expected)
+	assert.Equal(t, expected, actual, "Pub/SubPanic correct")
+}
+
+func TestPubSubRender(t *testing.T) {
+	bus := NewBus()
+	ok := false
+	bus.SubRender(func() {
+		ok = true
+	})
+	bus.PubRender()
+	assert.True(t, ok, "Pub/SubRender correct")
+}
+
+func TestPubSubTrace(t *testing.T) {
+	bus := NewBus()
+	expected := "tick"
+	var actual string
+	bus.SubTrace(func(topic Topic, _ interface{}) {
+		actual = topic.String()
+	})
+	bus.SubTick(func(counter int) {})
+	bus.PubTick(0)
+	assert.Equal(t, expected, actual, "SubTrace correct")
+}
+
+// TODO 🔥 we could call all of them, but ATM that's overkill
+// mostly, if they compile they're correct after these smoke tests
